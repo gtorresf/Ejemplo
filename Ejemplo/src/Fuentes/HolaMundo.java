34fcc70c09d1a6b9339b20b62bf761ba3fca9c05
SBPERU <giancarlo.415@gmail.com>
2014-10-06 17:08:06 -0500

    !

--- Ejemplo/src/Fuentes/HolaMundo.java
+++ Ejemplo/src/Fuentes/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Agregado desde la web!!!
+		// Added from the Github website!!!
 		System.out.println("Hola Mundo! [UPDATE]");
 	}
 
