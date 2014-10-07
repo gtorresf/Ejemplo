45ff5c5ed3b16d333ba9b628f3b3ef05e7d21864
SBPERU <giancarlo.415@gmail.com>
2014-10-07 15:07:08 -0500

    AV

--- Ejemplo/src/Forms/form1.java
+++ Ejemplo/src/Forms/form1.java
@@ -3,9 +3,6 @@ package Forms;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JComboBox;
-import java.awt.BorderLayout;
-import java.awt.Button;
 
 public class form1 {
 
@@ -41,12 +38,6 @@ public class form1 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//Agregando comentario
-		JComboBox comboBox = new JComboBox();
-		frame.getContentPane().add(comboBox, BorderLayout.WEST);
-		
-		Button button = new Button("New button");
-		frame.getContentPane().add(button, BorderLayout.SOUTH);
 	}
 
 }
