324be61e8ea69f88c795f506a47d042596f504fc
SBPERU <giancarlo.415@gmail.com>
2014-10-07 14:24:01 -0500

    Subiendo un formulario

--- Ejemplo/src/Clases/HolaMundo.java
+++ Ejemplo/src/Clases/HolaMundo.java
@@ -1,11 +1,11 @@
-package Fuentes;
+package Clases;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Added from the Github website!!!
-		System.out.println("Hola Mundo! [UPDATE]");
+		System.out.println("Hola Mundo! [UPDATE]"); 
 	}
 
 }
